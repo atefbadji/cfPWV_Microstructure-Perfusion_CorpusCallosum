a3cd1b84bafdeb4547be983b9bc89c2091022784
Agah <agahkarakuzu@gmail.com>
2019-10-10 08:41:26 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("ppcor")
+install.packages("ggplot2")
+install.packages("gridextra")
+install.packages("plotly")
+install.packages(c('repr', 'IRdisplay', 'pbdZMQ', 'devtools'))
+devtools::install_github('IRkernel/IRkernel')
+IRkernel::installspec()
+install.packages("olsrr") 
